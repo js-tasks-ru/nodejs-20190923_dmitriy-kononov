2432cda53e7e94076901cd795f88de036c1d4d23
Dmitriy Kononov <kondor_06@mail.ru>
2019-10-04 01:07:26 +0300

    start mod2-task1

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -3,13 +3,31 @@ const os = require('os');
 
 class LineSplitStream extends stream.Transform {
   constructor(options) {
+    const { encoding } = options;
+    
     super(options);
+
+    this._encoding = encoding;
+    this._buffer = '';
   }
 
   _transform(chunk, encoding, callback) {
+    console.log('---tr---');
+    this._buffer += chunk.toString(this._encoding);
+
+    console.log(`buffer = ${this._buffer}`)
+    callback();
   }
 
   _flush(callback) {
+    
+
+    this._buffer.split(os.EOL).forEach((str) => {
+      console.log('---fl----');
+      console.log(`str = ${str}`);
+      callback(null, str);
+    });
+
   }
 }
 
