062a951f75b20f597d2646c359fd1193a0122acb
Dmitriy Kononov <kondor_06@mail.ru>
2019-10-15 00:25:03 +0300

    add solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -7,10 +7,43 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const sessions = [];
+
+const subscription = (ctx) => {
+  return new Promise((resolve) => {
+    sessions.push(resolve);
+
+    ctx.res.on('close', () => {
+      sessions.splice(sessions.indexOf(resolve));
+    });
+  });
+};
+
+const distribution = (message) => {
+  sessions.forEach((el) => {
+    el(message);
+  });
+};
+
+
 router.get('/subscribe', async (ctx, next) => {
+  console.log('GET');
+
+  const message = await subscription(ctx);
+
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const {message} = ctx.request.body;
+
+  if (!message) {
+    ctx.body = '';
+    return;
+  }
+
+  distribution(message);
+  ctx.body = '';
 });
 
 app.use(router.routes());
