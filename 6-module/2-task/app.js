ade124217e4c163213ac8c7821ab4f6d901eacfb
Dmitriy <kondor_06@mail.ru>
2019-10-20 00:37:55 +0300

    6-module 2-task delete trash

--- 6-module/2-task/app.js
+++ 6-module/2-task/app.js
@@ -1,14 +1,9 @@
 const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
 const Router = require('koa-router');
 const {productsBySubcategory, productList, productById} = require('./controllers/products');
 const {categoryList} = require('./controllers/categories');
 
-const mongoose = require('mongoose');
-
 const app = new Koa();
-
-app.use(bodyParser());
 
 app.use(async (ctx, next) => {
   try {
@@ -30,61 +25,6 @@ const router = new Router({prefix: '/api'});
 router.get('/categories', categoryList);
 router.get('/products', productsBySubcategory, productList);
 router.get('/products/:id', productById);
-
-router.post('/add/', async (ctx) => {
-  let Model = await require('./models/Category');
-
-  for (let i = 0; i < 4; i++) {
-    await Model.create({
-      title: `GENERATED${i}`,
-      subcategories: [
-        {title: `SUB_CAT${i}`},
-        {title: `SUB_CAT2${i}`},
-      ],
-    });
-  }
-
-  await Model.create({
-    title: `GENERATED4`,
-  });
-
-  Model = await require('./models/Product');
-
-  await Model.create({
-    title: `PROD 1`,
-    description: 'dsdsdsdsdsds',
-    price: 33333,
-    category: new mongoose.Types.ObjectId('5dab5f53ff39122a09630ac0'),
-    subcategory: new mongoose.Types.ObjectId('5dab5f53ff39122a09630ac2'),
-  });
-
-  await Model.create({
-    title: `PROD 2`,
-    description: 'dsdsdsdsdsds',
-    price: 33333,
-    category: new mongoose.Types.ObjectId('5dab5f53ff39122a09630ac6'),
-    subcategory: new mongoose.Types.ObjectId('5dab5f53ff39122a09630ac7'),
-  });
-
-  await Model.create({
-    title: `PROD 4`,
-    description: 'dsdsdsdsdsds',
-    price: 33333,
-    category: new mongoose.Types.ObjectId('5dab5f53ff39122a09630ac3'),
-    subcategory: new mongoose.Types.ObjectId('5dab5f53ff39122a09630ac5'),
-  });
-
-  await Model.create({
-    title: `PROD 5`,
-    description: 'dsdsdsdsdsds',
-    price: 33333,
-    category: new mongoose.Types.ObjectId('5dab5f53ff39122a09630ac0'),
-    subcategory: new mongoose.Types.ObjectId('5dab5f53ff39122a09630ac2'),
-  });
-
-  ctx.status = 200;
-  ctx.body = 'OK';
-});
 
 app.use(router.routes());
 
