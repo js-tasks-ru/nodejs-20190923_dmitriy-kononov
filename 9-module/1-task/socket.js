477467443c09fff1ab493164d58b8684b3a0a3ad
Dmitriy <kondor_06@mail.ru>
2019-10-28 22:52:18 +0300

    9-module 1-task 1,2,3,5 is ready

--- 9-module/1-task/socket.js
+++ 9-module/1-task/socket.js
@@ -7,11 +7,33 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
+
+    if (!socket.handshake.query.token) {
+      return next(new Error('anonymous sessions are not allowed'));
+    }
+
+    const session = await Session.findOne({token: socket.handshake.query.token}).populate('user');
+
+    if (!session) {
+      return next(new Error('wrong or expired session token'));
+    }
+
+    socket.user = session._doc.user._doc;
+
     next();
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async (msg) => {
+      const message = new Message({
+        user: socket.user.displayName,
+        chat: socket.user._id,
+        text: msg,
+        date: new Date(),
+      });
+
+      message.save();
+    });
   });
 
   return io;
